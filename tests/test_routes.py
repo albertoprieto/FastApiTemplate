1229781dd3d8f4ec143a2ec978c9be482d01f3fd
JAPrieto <ja.prieto@mueblesamerica.mx>
2024-12-31 12:50:18 -0600

    Update tests and requirements

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,11 +1,4 @@
-import sys
-import os
-
-# Agregar el directorio raíz del proyecto al sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from fastapi.testclient import TestClient
-from app import app
 
 client = TestClient(app)
 
