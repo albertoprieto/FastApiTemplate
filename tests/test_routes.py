bc7d955c3602931d8915c7e311e0deef9c9ff676
JAPrieto <ja.prieto@mueblesamerica.mx>
2024-12-31 13:00:12 -0600

    Update tests and requirements

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -5,7 +5,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from fastapi.testclient import TestClient
-from .app import app
+from ..app import app
 
 client = TestClient(app)
 
